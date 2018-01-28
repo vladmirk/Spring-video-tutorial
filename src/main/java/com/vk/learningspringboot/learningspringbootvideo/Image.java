1c04e6a8a7297dfa8190f1bd0b92fc3deae77dcd
Vladmir <vkutnyakov@gmail.com>
2018-01-28 16:07:58 +0300

    Adding User/Owner Session security

--- src/main/java/com/vk/learningspringboot/learningspringbootvideo/Image.java
+++ src/main/java/com/vk/learningspringboot/learningspringbootvideo/Image.java
@@ -3,6 +3,7 @@ package com.vk.learningspringboot.learningspringbootvideo;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 /**
  * Created by vkutn on 20/12/2017.
@@ -15,11 +16,15 @@ public class Image {
     private long id;
     private String name;
 
+    @OneToOne
+    private User user;
+
     private Image() {
     }
 
-    public Image(String name) {
+    public Image(String name, User user) {
         this.name = name;
+        this.user = user;
     }
 
     public long getId() {
@@ -36,5 +41,13 @@ public class Image {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
--- src/main/java/com/vk/learningspringboot/learningspringbootvideo/ImageService.java
+++ src/main/java/com/vk/learningspringboot/learningspringbootvideo/ImageService.java
@@ -10,6 +10,7 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.FileSystemUtils;
@@ -28,27 +29,30 @@ import java.nio.file.Paths;
 public class ImageService {
 
     private static final String UPLOAD_ROOT = "upload-dir";
-    private final ImageRepository repository;
+    private final ImageRepository imageRepository;
     private final ResourceLoader resource;
     private final CounterService counterService;
     private final GaugeService gaugeService;
     private SimpMessagingTemplate simpMessagingTemplate;
+    private UserRepository userRepository;
 
     @Autowired
-    public ImageService(ImageRepository imageRepository, ResourceLoader resource, CounterService counterService, GaugeService gaugeService, SimpMessagingTemplate
-            simpMessagingTemplate) {
-        this.repository = imageRepository;
+    public ImageService(ImageRepository imageRepository, ResourceLoader resource, CounterService counterService,
+                        GaugeService gaugeService, SimpMessagingTemplate
+                                simpMessagingTemplate, UserRepository userRepository) {
+        this.imageRepository = imageRepository;
         this.resource = resource;
         this.counterService = counterService;
         this.gaugeService = gaugeService;
         this.simpMessagingTemplate = simpMessagingTemplate;
+        this.userRepository = userRepository;
 
         this.counterService.reset("files.uploaded");
         this.gaugeService.submit("files.uploaded.lastBytes", 0);
     }
 
     public Page<Image> findPage(Pageable pageable) {
-        return repository.findAll(pageable);
+        return imageRepository.findAll(pageable);
     }
 
     public Resource findOneImage(String name) {
@@ -62,7 +66,9 @@ public class ImageService {
     public void createImage(MultipartFile file) throws IOException {
         if (!file.isEmpty()) {
             Files.copy(file.getInputStream(), Paths.get(UPLOAD_ROOT, file.getOriginalFilename()));
-            repository.save(new Image(file.getOriginalFilename()));
+            imageRepository.save(new Image(file.getOriginalFilename(),
+                    userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName())
+            ));
             counterService.increment("files.uploaded");
             gaugeService.submit("files.uploaded.lastBytes", file.getSize());
             simpMessagingTemplate.convertAndSend("/topic/newImage", file.getOriginalFilename());
@@ -70,8 +76,8 @@ public class ImageService {
     }
 
     public void deleteImage(String name) throws IOException {
-        final Image byName = repository.findByName(name);
-        repository.delete(byName);
+        final Image byName = imageRepository.findByName(name);
+        imageRepository.delete(byName);
         Files.deleteIfExists(Paths.get(UPLOAD_ROOT, name));
         simpMessagingTemplate.convertAndSend("/topic/deleteImage", name);
     }
@@ -86,17 +92,18 @@ public class ImageService {
 
         return (args) -> {
 
-            userRepository.save(new User("user1", "pass", "USER"));
+            User user1 = userRepository.save(new User("user1", "pass", "USER"));
+            User user2 = userRepository.save(new User("user2", "pass", "USER", "OPERATOR"));
 
 
             FileCopyUtils.copy("Test file", new FileWriter(UPLOAD_ROOT + "/test"));
-            imageRepository.save(new Image("test"));
+            imageRepository.save(new Image("test", user1));
 
             FileCopyUtils.copy("Test file2", new FileWriter(UPLOAD_ROOT + "/test2"));
-            imageRepository.save(new Image("test2"));
+            imageRepository.save(new Image("test2", user1));
 
             FileCopyUtils.copy("Test file3", new FileWriter(UPLOAD_ROOT + "/test3"));
-            imageRepository.save(new Image("test3"));
+            imageRepository.save(new Image("test3", user2));
         };
     }
 }
